c7c23ba1f260500d999961c136d09f3eed35f342
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2023-02-09 09:03:16 -0500

    part 2

---
+++ SuperDir/FileInfo_H.h
@@ -0,0 +1,19 @@
+#pragma once
+class FileInfo_H
+{
+	virtual void Displayinformation();
+
+public:
+	FileInfo_H();
+	~FileInfo_H();
+
+private:
+};
+
+FileInfo_H::FileInfo_H()
+{
+}
+
+FileInfo_H::~FileInfo_H()
+{
+}
